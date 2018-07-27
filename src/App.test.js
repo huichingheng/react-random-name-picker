36b9d53328681659f806900300ea4c67396a7331
huichingheng <huichingheng@gmail.com>
2018-07-27 17:53:30 +0800

    "added test - renders div with some text"

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ShallowRender from "react-test-renderer/shallow";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders div with some text", () => {
+  const renderer = new ShallowRender();
+  renderer.render(<App />);
+  const output = renderer.getRenderOutput();
+
+  expect(output).toMatchSnapshot();
 });
